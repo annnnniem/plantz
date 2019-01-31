297755c306dd6445657a63ace65772907d5210e7
Annie Martens <anniecmartens@gmail.com>
2019-01-31 12:03:30 -0600

    update segment source

--- public/index.js
+++ public/index.js
@@ -87,8 +87,9 @@ $('#boot_random_user').click(function(){
 
 $('#segment_identify_user').click(function(){
 	   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-  		analytics.load("jdqeEX1rsEsl5UmjSVbVjILjGf1f7TeK");
-  		analytics.page();
+		  analytics.load("J7lxzsNRvtQKKF3TROD04lo9WBjfyYNb");
+		  analytics.page();
+		  }}();
  }}();
 
 	analytics.identify(faker.random.alphaNumeric, {
